c43c49fc12032b26b27c893ea45cdac5c3c6b74b
mahesh <mail2mahesh0537@gmail.com>
2023-05-06 15:42:52 +0530

    bs

--- testBench/branch-2.s
+++ testBench/branch-2.s
@@ -7,4 +7,4 @@ ndu r3, r2, r1   ; set everything except the last 2 bits to 1
 ndu r3, r3, r3  
 
 beq r3, r5, -3  ; this branch is taken every 4th iter, so prediction should always be not taken
-blt r4, r1, -4   ; prediction should be taken, except in 1st iter
+blt r1, r4, -4   ; prediction should be taken, except in 1st iter
